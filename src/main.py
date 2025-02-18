8743c6ecf2ae0614be427d6c265198618f2f2fc3
Juan Acosta <jmjlacosta@gmail.com>
2025-02-18 14:58:29 -0600

    Update main.py
    removed unnecessary stuff

--- src/main.py
+++ src/main.py
@@ -5,35 +5,22 @@ import time
 from fastapi import FastAPI, UploadFile, File
 from typing import Annotated
 from pydantic import BaseModel, Field
+from fastapi.middleware.cors import CORSMiddleware
 
-from src.api_diagnostics.router import router as diagnostic_router
-from src.api_diagnostics.schemas import ExceptionMessage
-from src.chads_vasc_score.router import router as cvs_router
-from src.config import settings
-
-# It's good practice to set default server timezone to UTC for standardization.
-os.environ["TZ"] = "UTC"
-time.tzset()
 
 app = FastAPI(
-    title=settings.TITLE,
-    description=settings.DESCRIPTION,
-    debug=settings.DEBUG,
-    version=settings.VERSION,
-    openapi_url=settings.OPEN_API_URL,
-    swagger_ui_parameters={"persistAuthorization": True},
-    responses={
-        400: {"model": ExceptionMessage},
-        401: {"model": ExceptionMessage},
-        403: {"model": ExceptionMessage},
-        404: {"model": ExceptionMessage},
-        422: {"model": ExceptionMessage},
-    },
+    title="Multiple File Upload",
+    description="Multiple file upload test",
+    version="1.0.0",
 )
 
-app.include_router(diagnostic_router, tags=["Diagnostics"])
-app.include_router(cvs_router, prefix=settings.API_URL, tags=["CHA₂DS₂-VASc"])
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class TestOutput(BaseModel):
     """Form-based output schema for file upload test."""
